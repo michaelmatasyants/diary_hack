0d615fcc58f6015fde5804bcd85f1d477a69ab70
Michael Matasyants <michael.matasyanc@gmail.com>
2023-06-09 12:37:01 +0300

    Optimize queries

--- scripts.py
+++ scripts.py
@@ -27,22 +27,29 @@ def remove_chastisements(schoolkid: Schoolkid):
     Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(subject_name: str, schoolkid: Schoolkid):
+def create_commendations(subject_names: list, schoolkid: Schoolkid):
     '''Finds the last lesson in a given subject for a specified student
     and creates a praise, randomly selected from a list of praises'''
-    subject = Subject.objects.filter(title__icontains=subject_name,
-                                     year_of_study=schoolkid.year_of_study
-                                     ).first()
-    random_praise = choice(WORDS_OF_PRAISE)
-    last_lesson = Lesson.objects.select_related('teacher').filter(
-        group_letter=schoolkid.group_letter,
-        year_of_study=schoolkid.year_of_study,
-        subject__title=subject.title).order_by('-date').first()
-    Commendation.objects.create(text=random_praise,
-                                created=last_lesson.date,
-                                schoolkid=schoolkid,
-                                subject=subject,
-                                teacher=last_lesson.teacher)
+    all_subjects = Subject.objects.filter(year_of_study=schoolkid.year_of_study)
+    subjects_queryset = []
+    for subject_name in subject_names:
+        for subject in all_subjects:
+            if subject_name in subject.title:
+                subjects_queryset.append(subject)
+                continue
+    if not subjects_queryset:
+        return print('There are no such subjectss.')
+    for subject in subjects_queryset:
+        random_praise = choice(WORDS_OF_PRAISE)
+        last_lesson = Lesson.objects.select_related('teacher').filter(
+            group_letter=schoolkid.group_letter,
+            year_of_study=schoolkid.year_of_study,
+            subject__title=subject.title).order_by('-date').first()
+        Commendation.objects.create(text=random_praise,
+                                    created=last_lesson.date,
+                                    schoolkid=schoolkid,
+                                    subject=subject,
+                                    teacher=last_lesson.teacher)
 
 
 def main():
@@ -71,8 +78,7 @@ def main():
         'Литература', 'Обществознание', 'Иностранный язык', 'Биология',
         'История', 'ОБЖ'
     ]
-    for subject_name in subject_names:
-        create_commendation(subject_name=subject_name, schoolkid=child)
+    create_commendations(subject_names=subject_names, schoolkid=child)
     print("За последний урок по каждому из предметов была добавлена похвала",
           "от учителя.")
 
